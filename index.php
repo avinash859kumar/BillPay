cb42b8a9e3cd0ab2f15dcd2ab58240bbd2c043bb
Dayfi <avinash859kumar@gmail.com>
2020-05-24 10:10:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Bill Pay</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <script src="https://kit.fontawesome.com/7cfc17a9b4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="../pay/favicon.ico" type="image/x-icon">
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+</head>
+<body id="background">
+    <header>
+        <img src="images/BPS.png" >
+    </header>
+    <nav>
+        <ul>
+            <li class="active" class="selected"><a href="index.php" >home</a></li>
+            <li  ><a href="about.php">about</a></li>
+            <li><a href="login.php">login</a></li>
+            <li><a href="signup.php">sign up</a></li>
+            <li><a href="contact.php">contact</a></li>
+        </ul>
+    </nav>
+<!-- Slideshow container -->
+<div class="hero-image">
+
+       <img src="images/google-play-icon.png""" alt="Google Play Store" >
+       <img src="images/app-store-icon.png" alt="App Store">
+
+       <img src="images/ipho.png" style="height: 600px;background-image: url(../images/background.jpg);padding: 22px;margin: 10px 20px 40px;" alt="App Store">
+</div>
+
+<div class="sec1">
+
+					<h1>Bill Pay</h1>
+					<p>
+            No need to rush to the market for DTH Recharge or Mobile Bill Payment, just login to BillPay and make immediate payments.<br>
+            Cherish your experience with exciting Cashback and Discounts on every recharge or bill payment you make with us.<br>
+            Our online recharge and bill payment service is a one-stop solution for fast & easy Prepaid Mobiles,<br>
+            DTH & Data Card recharge and Postpaid Mobiles & Data Cards Bill Payment. Effortlessly make payments through our safe methods, processed through secured gateways.<br>
+					</p>
+</div>
+
+<div class="container1">
+  <div class="row">
+    <div class="col-md-4" >
+      <h1><span class="counter">992</span></h1>
+      <h3>Customers</h3>
+      <i class="fa fa-users"></i>
+    </div>
+    <div class="col-md-4">
+      <h1><span class="counter">3,075</span></h1>
+      <h3>Total Web Visits</h3>
+      <i class="fa fa-desktop"></i>
+    </div>
+    <div class="col-md-4">
+      <h1><span class="counter">1,218</span></h1>
+      <h3>Cups Of Coffee</h3>
+      <i class="fa fa-coffee"></i>
+    </div>
+  </div>
+</div>
+
+
+
+   <div class="footer">
+        <div class="footer-content">
+
+          <div class="footer-section">
+          <div class="contact">
+            <span><i class="fas fa-phone"></i>&nbsp; 123-456-789</span>
+            <span><i class="fas fa-envelope"></i>&nbsp; info@billpay.com</span>
+          </div>
+           <div class="socials">
+            <a href="#"><i class="fab fa-facebook" style="color:#3b5998;font-size: 48px;"></i></a>
+            <a href="#"><i class="fab fa-instagram" style="color: #3f729b;font-size: 48px;"></i></a>
+            <a href="#"><i class="fab fa-twitter" style="color:  #55acee;font-size: 48px;"></i></a>
+            <a href="#"><i class="fab fa-youtube" style="color:  #e52d27;font-size: 48px;"></i></a>
+          </div>
+          </div>
+          <div class="footer-section links">
+            <h2>DTH Recharges</h2>
+
+            <ul>
+              <a href="login.php">
+                <li>Airtel</li>
+              </a>
+              <a href="login.php">
+                <li>Tata Sky</li>
+              </a>
+              <a href="login.php">
+              <li>Dish Tv</li>
+              </a>
+              <a href="login.php">
+              <li>Videocon</li>
+              </a>
+              <a href="login.php">
+              <li>Zippytel</li>
+              </a>
+              <a href="login.php">
+              <li>Free dish</li>
+              </a>
+            </ul>
+
+          </div>
+          <div class="footer-section links2">
+
+          <h2>Mobile Recharges</h2>
+
+            <ul>
+              <a href="login.php">
+                <li>Airtel</li>
+              </a>
+              <a href="login.php">
+                <li>Vodafone</li>
+              </a>
+              <a href="login.php">
+              <li>Jio</li>
+              </a>
+              <a href="login.php">
+              <li>Idea</li>
+              </a>
+              <a href="login.php">
+              <li>BSNL</li>
+              </a>
+              <a href="login.php">
+              <li>Telenor</li>
+              </a>
+            </ul>
+
+        </div>
+        <div class="footer-bottom">
+          &copy;  Bill Pay 
+        </div>
+      </div>
+
+
+
+}
+
+
+        </script>
+         <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script>
+jQuery(document).ready(function($)
+{
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+$('.counter').addClass('animated fadeInDownBig');
+$('h3').addClass('animated fadeIn');
+$(document).ready(function(){
+
+});
+});
+</script>
+</body>
+</html>
